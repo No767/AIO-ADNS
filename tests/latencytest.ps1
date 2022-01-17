c3a5e00db384d2670d242249e8ac528fe774349b
Isaac-To <isaacto3890@gmail.com>
2022-01-17 15:08:03 -0800

    include more dns tests

---
+++ tests/latencytest.ps1
@@ -0,0 +1,20 @@
+﻿#####################Variables#####################
+$numberoftests = 10
+###################################################
+#####################Main#####################
+cls
+$dnsserver = "127.0.0.1"
+$totalmeasurement = 0
+$i = 0
+while ($i -ne $numberoftests)
+{
+    $measurement = (Measure-Command {Resolve-DnsName www.bing.com -Server $dnsserver –Type A}).TotalSeconds
+
+    $totalmeasurement += $measurement
+
+    $i += 1
+}
+$totalmeasurement = $totalmeasurement / $numberoftests
+"DNS Server: " + $dnsserver + ", Response time: " + $totalmeasurement + " seconds"
+
+#Script from https://social.technet.microsoft.com/wiki/contents/articles/38126.how-to-measure-the-response-time-of-dns-servers-when-performing-name-server-lookups.aspx
