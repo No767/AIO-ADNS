7385213f342f01261fc1e8cff7d04e46b117b3c4
No767 <73260931+No767@users.noreply.github.com>
2022-06-23 15:59:16 -0700

    Add AIO-ADNS-React

---
+++ AIO-ADNS-React/src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { IndexBody } from "../shared/index-components/body";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <IndexBody />
+  </React.StrictMode>
+)
